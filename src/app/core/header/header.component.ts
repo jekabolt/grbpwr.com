a822514adfd0fdb304b3ccdd1f5633032d8afe6e
TarasovIlia <tarasov002@gmail.com>
2021-09-03 19:22:47 +0300

    optimized header scss and html

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,22 +1,50 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import { Component, OnDestroy, OnInit, HostListener } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
+import {CartService} from '../../cart/shared/cart.service';
 
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.style.scss/header.component.scss']
 })
 export class HeaderComponent implements OnInit, OnDestroy {
-  constructor() {}
-
-  ngOnInit() {
-  }
-
+  public navbarOpened = false
+  public infoOpened = false
   public onMenuToggle(e: Event) {
     e.preventDefault();
   }
 
-  ngOnDestroy() {
+
+  constructor(
+              public cartService: CartService,
+              private router: Router) {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      this.hideNavbar()
+    });
   }
 
+  toggleNavbar() {
+    this.navbarOpened = !this.navbarOpened;
+  }
+
+  hideNavbar() {
+    this.navbarOpened = false;
+  }
+
+  @HostListener('window:keydown.escape', ['$event'])
+  keyEvent(event: KeyboardEvent) {
+    this.hideNavbar()
+  }
+
+  ngOnInit() {
+
+  }
+
+  ngOnDestroy() {
+
+  }
 }
