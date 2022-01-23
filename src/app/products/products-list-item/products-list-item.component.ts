3f9cf9d02fd4e83d4ed73dc0060fcc93420773ff
jekabolt <jekabolt@yahoo.com>
2022-01-24 01:17:02 +0300

    cureencies

--- src/app/products/products-list-item/products-list-item.component.ts
+++ src/app/products/products-list-item/products-list-item.component.ts
@@ -3,6 +3,7 @@ import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 
 import { CartService } from '../../cart/shared/cart.service';
+import { LocaleService } from '../../services/locale-storage.service'
 
 import { CartItem } from '../../models/cart-item.model';
 import { Product } from '../../models/product.model';
@@ -17,12 +18,19 @@ export class ProductsListItemComponent implements OnInit, OnDestroy {
   @Input() public product: Product;
   @Input() public displayMode: string;
   public imageLoading: boolean;
+  public currencySelected: string;
+  public usd: boolean;
+  public eur: boolean;
+  public rub: boolean;
 
   constructor(
     private cartService: CartService,
+    public localeService: LocaleService,
   ) { }
 
   ngOnInit() {
+    this.currencySelected = this.localeService.getCurrency()
+    this.setCurrency(this.currencySelected)
     this.imageLoading = true;
   }
 
@@ -36,6 +44,30 @@ export class ProductsListItemComponent implements OnInit, OnDestroy {
 
   public onImageLoad() {
     this.imageLoading = false;
+  }
+  
+  setCurrency(currency){
+    switch (currency) {
+      case "usd":
+          this.usd = true
+          this.eur = false
+          this.rub = false
+          break;
+      case "eur":
+          this.usd = false
+          this.eur = true
+          this.rub = false
+          break;
+      case "rub":
+          this.usd = false
+          this.eur = false
+          this.rub = true
+          break;
+      default:
+          console.log("No such currency exists!");
+          this.usd = true
+          break;
+    }
   }
 
   ngOnDestroy() {
