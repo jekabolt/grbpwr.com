7fc1d19a8caf903ba5152b60271e597c8adf4ade
maksim hodasevich <maksim.hodasevich@gmail.com>
2024-03-19 20:58:41 +0100

    init next.js project

---
+++ src/app/catalog/[productId]/page.tsx
@@ -0,0 +1,14 @@
+export default async function ProductPage({ params }: any) {
+  const data = await fetch(
+    `http://worldtimeapi.org/api/timezone/Europe/${params.productId}`,
+    {
+      next: {
+        revalidate: 60,
+      },
+    }
+  );
+
+  const data1 = await data.json();
+
+  return <main>{JSON.stringify(data1)}</main>;
+}
