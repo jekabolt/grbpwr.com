b84169dc9733171c1602b59ee59605221c4887ae
maksim hodasevich <maksim.hodasevich@gmail.com>
2024-04-11 19:56:54 +0200

    feature/add products to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,18 +7,18 @@ export default async function Page() {
 
   if (!hero) return "no hero data";
 
-  const { id, createdAt, main, ads } = hero;
+  const { id, createdAt, main, ads, productsFeatured } = hero;
 
   return (
     <main className="p-2">
       <div>
-        <span className="text-xl font-bold">
+        <span className="font-bold">
           revalidate time: <span className="text-orange-400">15 seconds</span>
         </span>{" "}
         {`id: ${id} createdAt: ${createdAt}`}
       </div>
-      <div className="flex flex-col gap-10 md:flex-row">
-        <div>
+      <div className="flex flex-col gap-10">
+        <div className="mx-auto">
           <span className="text-2xl font-bold">hero.main</span>
           <div className="space-y-2">
             <Image
@@ -38,7 +38,7 @@ export default async function Page() {
               )}
           </div>
         </div>
-        <div className="space-y-10">
+        <div className="mx-auto space-y-10">
           <span className="text-2xl font-bold">hero.ads</span>
           {ads &&
             ads.map((ad, i) => (
@@ -55,6 +55,32 @@ export default async function Page() {
                       key !== "contentLink" && (
                         <p key={key}>
                           <span className="font-bold">{key}</span>: {value}
+                        </p>
+                      ),
+                  )}
+              </div>
+            ))}
+        </div>
+        <div className="mx-auto space-y-10">
+          <span className="text-2xl font-bold">hero.productsFeatured</span>
+          {productsFeatured &&
+            productsFeatured.map((product, i) => (
+              <div key={i} className="space-y-2">
+                <Image
+                  src={product?.productInsert?.thumbnail || ""}
+                  alt="ad hero image"
+                  height={200}
+                  width={200}
+                />
+                {product.productInsert &&
+                  Object.entries(product.productInsert).map(
+                    ([key, value]) =>
+                      key !== "contentLink" && (
+                        <p key={key}>
+                          <span className="font-bold">{key}</span>:{" "}
+                          {typeof value === "string"
+                            ? value
+                            : JSON.stringify(value)}
                         </p>
                       ),
                   )}
