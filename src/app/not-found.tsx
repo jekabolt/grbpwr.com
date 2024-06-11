e3c83c4c9ca6a1762f89e82bc8747038e0d07798
Mike Velko <44818580+mikevelko@users.noreply.github.com>
2024-06-12 00:32:32 +0200

    not found page (#88)
    * npt found page
    
    * apos fix

---
+++ src/app/not-found.tsx
@@ -0,0 +1,30 @@
+import Header from "@/components/global/Header";
+import Button from "@/components/ui/Button";
+import { ButtonStyle } from "@/components/ui/Button/styles";
+import Link from "next/link";
+import "./globals.css";
+
+export default function NotFound() {
+  return (
+    <div>
+      <Header />
+      <div className="ml-40 mt-40">
+        <p className="mb-4">page not found.</p>
+        <p className="mb-6">
+          sorry, the page you are looking for doesn&apos;t exist or has been
+          moved.
+          <br />
+          please go back to the homepage or contact us if the problem persists.
+        </p>
+        <div className="flex gap-x-20">
+          <Button asChild style={ButtonStyle.underlinedButton}>
+            <Link href="/contacts">contact us</Link>
+          </Button>
+          <Button asChild style={ButtonStyle.simpleButton}>
+            <Link href="/catalog">catalog</Link>
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
