49a05a5e1010eb3393bbb38dbd284ae23be1cd2b
maksim hodasevich <47758224+dogfrogfog@users.noreply.github.com>
2024-07-10 21:13:18 +0200

    add hero context (#108)

---
+++ src/components/global/Footer/CurrencyPopover.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { useHeroContext } from "@/components/contexts/HeroContext";
+import GenericPopover from "@/components/ui/Popover";
+import { cn } from "@/lib/utils";
+
+export default function Component() {
+  const { rates, selectedCurrency, setSelectedCurrency } = useHeroContext();
+
+  if (!rates?.currencies) return null;
+
+  return (
+    <GenericPopover
+      title="currency"
+      openElement={
+        <span className="bg-textColor px-2 py-1 text-buttonTextColor">
+          {`Currency: ${selectedCurrency}`}
+        </span>
+      }
+    >
+      <div className="space-y-2 px-12 pb-7">
+        {Object.entries(rates.currencies).map(([k, v]) => (
+          <button
+            key={k}
+            onClick={() => {
+              setSelectedCurrency(k);
+            }}
+            className={cn("flex items-center gap-1 text-sm", {
+              underline: k === selectedCurrency,
+            })}
+          >
+            {v.description} {JSON.stringify(v.rate)}
+          </button>
+        ))}
+      </div>
+    </GenericPopover>
+  );
+}
