7e3d46fca9c37671c01227202b988546d0cafef8
maksim hodasevich <maksim.hodasevich@gmail.com>
2024-10-20 17:45:42 +0200

    remove invoice page, move cryprto patment QR code to  order page

--- src/app/order/[uuid]/page.tsx
+++ src/app/order/[uuid]/page.tsx
@@ -1,6 +1,8 @@
 import { serviceClient } from "@/lib/api";
+import QRCode from "qrcode";
 
-import V0GenUi from "./V0GenUi";
+import V0GenUiOrder from "./V0GenUi";
+import V0GenUiQRCode from "./V0GenUiQRCode";
 
 interface Props {
   params: {
@@ -13,9 +15,39 @@ export default async function OrderPage({ params }: Props) {
 
   const response = await serviceClient.GetOrderByUUID({ orderUuid: uuid });
 
+  const isCryptoPayment =
+    response.order?.payment?.paymentInsert?.paymentMethod ===
+    "PAYMENT_METHOD_NAME_ENUM_USDT_SHASTA";
+  const isTransactionDone =
+    response.order?.payment?.paymentInsert?.isTransactionDone;
+
+  if (isCryptoPayment && !isTransactionDone) {
+    const qrBase64Code = await QRCode.toDataURL(
+      response.order?.payment?.paymentInsert?.payee || "",
+    );
+
+    return (
+      <div>
+        <V0GenUiQRCode
+          qrBase64Code={qrBase64Code}
+          euroAmount={
+            response.order?.payment?.paymentInsert?.transactionAmount?.value ||
+            ""
+          }
+          cryptoAmount={
+            response.order?.payment?.paymentInsert
+              ?.transactionAmountPaymentCurrency?.value || ""
+          }
+          orderId={uuid}
+          payeeAddress={response.order?.payment?.paymentInsert?.payee || ""}
+        />
+      </div>
+    );
+  }
+
   return (
     <div>
-      <V0GenUi orderData={response.order as any} />
+      <V0GenUiOrder orderData={response.order as any} />
     </div>
   );
 }
