59a001f5b17e9083d735081ae16d7ced34940af2
maksim hodasevich <maksim.hodasevich@gmail.com>
2024-10-25 18:16:40 +0200

    delete files

--- src/features/catalog-filters/Size.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { useHeroContext } from "@/components/contexts/HeroContext";
-
-export default function Size({ sizeId }: { sizeId: number }) {
-  const { dictionary } = useHeroContext();
-
-  return <>{dictionary?.sizes?.find((size) => size.id === sizeId)?.name}</>;
-}
