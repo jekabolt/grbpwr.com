536e768e3dfdd8b8c811a5e0d81a6770fcf7348f
maksim hodasevich <maksim.hodasevich@gmail.com>
2024-11-01 17:00:35 +0100

    add working client side popuip cart, add to cart functionality, persist store to local storage

--- src/app/(checkout)/cart/_components/CartProductsList.tsx
+++ src/app/(checkout)/cart/_components/CartProductsList.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 // import { addCartProduct, clearCartProducts } from "@/lib/actions/cart";
-import { use } from "react";
+import { cache, useEffect, useState } from "react";
+import { ValidateOrderItemsInsertResponse } from "@/api/proto-http/frontend";
 
 import { serviceClient } from "@/lib/api";
 import { useCart } from "@/lib/stores/cart/store-provider";
 import CartItemRow from "@/app/(checkout)/cart/_components/CartItemRow";
+
+import SelectedCurrency from "./TotalPrice";
 
 // import SelectedCurrency from "@/app/(checkout)/cart/_components/TotalPrice/SelectedCurrency";
 // import { getValidateOrderItemsInsertItems } from "@/app/(checkout)/cart/_components/utils";
@@ -17,66 +20,38 @@ export default function CartProductsList({
 }: {
   className?: string;
 }) {
+  const [cartItemFullResponse, setCartItemFullResponse] = useState<
+    ValidateOrderItemsInsertResponse | undefined
+  >(undefined);
   const products = useCart((state) => state.products);
   console.log(products);
-  // const response = use(
 
-  //   serviceClient.ValidateOrderItemsInsert({
-  //     items: [],
-  //     shipmentCarrierId: undefined,
-  //     promoCode: undefined,
-  //   }),
-  // );
+  useEffect(() => {
+    async function fetchData() {
+      const response = await serviceClient.ValidateOrderItemsInsert({
+        items: products.map((p) => ({
+          productId: p.id,
+          quantity: p.quantity,
+          sizeId: p.size,
+        })),
+        shipmentCarrierId: undefined,
+        promoCode: undefined,
+      });
 
-  // console.log("responseresponse");
-  // console.log(response);
+      setCartItemFullResponse(response);
+    }
 
-  // const items = await getValidateOrderItemsInsertItems();
+    fetchData();
+  }, [products]);
 
-  // if (items.length === 0) return null;
-
-  // const response = await serviceClient.ValidateOrderItemsInsert({
-  //   items,
-  //   shipmentCarrierId: undefined,
-  //   promoCode: undefined,
-  // });
-
-  // const updateCookieCart = async () => {
-  //   "use server";
-
-  //   if (response?.validItems && response?.validItems?.length > 0) {
-  //     clearCartProducts();
-
-  //     for (const p of response.validItems) {
-  //       if (
-  //         p.orderItem?.productId &&
-  //         p.orderItem.quantity &&
-  //         p.orderItem.sizeId
-  //       ) {
-  //         addCartProduct({
-  //           id: p.orderItem.productId,
-  //           size: p.orderItem.sizeId.toString(),
-  //           quantity: p.orderItem.quantity,
-  //         });
-  //       }
-  //     }
-  //   }
-  // };
+  console.log(cartItemFullResponse);
 
   return (
     <div className={"space-y-6"}>
-      {JSON.stringify(products)}
-      {/* {response?.validItems?.map((p, i) => (
-        <CartItemRow
-          key={(p.id as number) + i}
-          product={p}
-          className={className}
-        />
-      ))} */}
-      {/* <SelectedCurrency baseCurrencyTotal={Number(response.totalSale?.value)} /> */}
-      {/* {response.hasChanged && ( */}
-      {/* <HACK__UpdateCookieCart updateCookieCart={updateCookieCart} /> */}
-      {/* )} */}
+      {cartItemFullResponse?.validItems?.map((p) => (
+        <CartItemRow key={p.slug} product={p} className={className} />
+      ))}
+      <SelectedCurrency />
     </div>
   );
 }
