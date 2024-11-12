f8ebc48367da4f02e9debc7f4ee740101e6c11ec
maksim hodasevich <maksim.hodasevich@gmail.com>
2024-11-12 20:52:13 +0100

    add loading skeleton

--- src/app/(checkout)/order/[uuid]/page.tsx
+++ src/app/(checkout)/order/[uuid]/page.tsx
@@ -1,8 +1,12 @@
 import { Suspense } from "react";
+import Link from "next/link";
 
 import { serviceClient } from "@/lib/api";
+import { Button } from "@/components/ui/button";
+import { Text } from "@/components/ui/text";
 
 import { OrderPageComponent } from "./_components/order-page";
+import { OrderPageSkeleton } from "./_components/order-page-skeleton";
 
 interface Props {
   params: Promise<{
@@ -47,8 +51,26 @@ export default async function OrderPage(props: Props) {
   // }
 
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <OrderPageComponent orderPromise={orderPromise} />
-    </Suspense>
+    <div className="flex min-h-screen flex-col justify-between gap-40 px-16 pb-10 pt-20">
+      <div>
+        <Text variant="uppercase" component="h1">
+          Order info
+        </Text>
+
+        <Suspense fallback={<OrderPageSkeleton />}>
+          <OrderPageComponent orderPromise={orderPromise} />
+        </Suspense>
+      </div>
+      <div>
+        {/* // add button style based on text */}
+        <Button variant="default" size="sm" asChild>
+          <Link href="/">
+            <Text component="span" size="small" variant="inactive">
+              contact support
+            </Text>
+          </Link>
+        </Button>
+      </div>
+    </div>
   );
 }
