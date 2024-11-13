bd4a94ff9761397de3e02bfde500736ee16221f2
maksim hodasevich <47758224+dogfrogfog@users.noreply.github.com>
2024-11-13 17:03:29 +0100

    feature/add newsletter unsub and 404 pages (#145)

---
+++ src/app/newsletter/unsubscribe/page.tsx
@@ -0,0 +1,21 @@
+import { Button } from "@/components/ui/button";
+import { Text } from "@/components/ui/text";
+import { Header } from "@/app/_components/header";
+
+export default function Unsubscribe() {
+  return (
+    <div>
+      <Header />
+      <div className="space-y-6 px-2.5 pt-6 lg:px-24 lg:pt-0">
+        <Text className="lg:max-w-xl">
+          we are so sorry you are leaving. click &quot;unsubscribe&quot; to
+          resign from list &quot;grbpwr&quot;.by clicking unsubscribe,
+          you&apos;ll no longer receive marketing emails.
+        </Text>
+        <Button variant="main" size="lg">
+          unsubscribe
+        </Button>
+      </div>
+    </div>
+  );
+}
