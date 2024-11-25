aebffda9392eef2df318baaa7bba96e2a91403d9
oak <dubodeldanila@gmail.com>
2024-11-25 18:22:55 +0300

    privacy policy page created

--- src/components/ui/cookie-banner.tsx
+++ src/components/ui/cookie-banner.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 import { Text } from "@/components/ui/text";
 
 import { Button } from "./button";
+import CareComposition from "./care-composition";
 import RadioGroupComponent from "./radio-group";
 
 const cookieOptions = [
@@ -12,42 +13,57 @@ const cookieOptions = [
   { label: "deny all", value: "deny" },
 ];
 
-export function CookieBanner() {
+interface Props {
+  isPrivacyPage?: boolean;
+}
+
+export function CookieBanner({ isPrivacyPage = false }: Props) {
   const [isVisible, setIsVisible] = useState(false);
   const [consent, setConsent] = useState<"accept" | "deny" | null>(null);
+  const baseStyles = isPrivacyPage
+    ? "space-y-6"
+    : "fixed bottom-0 left-0 right-0 z-50 space-y-6 bg-white p-4";
 
   useEffect(() => {
-    const consent = localStorage.getItem("cookieConsent");
-    if (!consent) {
+    const savedConsent = localStorage.getItem("cookieConsent");
+    if (savedConsent) {
+      setConsent(savedConsent === "true" ? "accept" : "deny");
+    }
+    if (!savedConsent || isPrivacyPage) {
       setIsVisible(true);
     }
-  }, []);
+  }, [isPrivacyPage]);
 
-  if (!isVisible) return null;
+  if (!isVisible && !isPrivacyPage) return null;
 
   const handleConsent = (value: string) => {
     setConsent(value as "accept" | "deny");
   };
 
   const handleSubmit = () => {
-    if (consent === "accept") {
-      localStorage.setItem("cookieConsent", "true");
-    } else {
-      localStorage.setItem("cookieConsent", "false");
-    }
+    if (!consent) return;
+    localStorage.setItem(
+      "cookieConsent",
+      consent === "accept" ? "true" : "false",
+    );
     setIsVisible(false);
   };
 
   return (
-    <div className="fixed bottom-0 left-0 right-0 z-50 space-y-6 bg-white p-4">
+    <div className={baseStyles}>
       <div>
-        <Text variant="uppercase">managing your data preferences</Text>
+        <Text variant="uppercase">
+          {isPrivacyPage
+            ? "7. managing your data preferences"
+            : "managing your data preferences"}
+        </Text>
       </div>
       <div>
         <Text>
           you can manage your preferences regarding your data trough our website
         </Text>
       </div>
+      {isPrivacyPage && <CareComposition className="space-y-6" />}
       <RadioGroupComponent
         name="cookieConsent"
         items={cookieOptions}
