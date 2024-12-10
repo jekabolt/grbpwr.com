27448065094373d45546b328ca3c1297431d5f93
maksim hodasevich <maksim.hodasevich@gmail.com>
2024-12-10 17:49:11 +0100

    add images to popup cart and black theme

--- src/app/(checkout)/cart/_components/CartPopup.tsx
+++ src/app/(checkout)/cart/_components/CartPopup.tsx
@@ -22,10 +22,10 @@ export default function CartPopup({ children }: { children: React.ReactNode }) {
       <Button onClick={() => setOpenStatus((v) => !v)} variant="main">
         cart {itemsQuantity ? `(${itemsQuantity})` : ""}
       </Button>
-      <div className="">
+      <div className="blackTheme">
         <div
           className={cn(
-            "absolute -top-1 right-0 z-30 hidden w-[500px] space-y-6 bg-bgColor p-2.5",
+            "absolute right-0 top-10 z-30 hidden w-[500px] space-y-6 bg-bgColor p-2.5",
             {
               block: open,
             },
@@ -40,7 +40,7 @@ export default function CartPopup({ children }: { children: React.ReactNode }) {
           </div>
           {children}
           <Button asChild variant="main" size="lg" className="block w-full">
-            <Link href="/checkout">PROCEED TO CHECKOUT</Link>
+            <Link href="/checkout">GO TO CHECKOUT</Link>
           </Button>
         </div>
       </div>
