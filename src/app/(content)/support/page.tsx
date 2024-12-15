843862c9c9930652e6e18a4e6bd3df0cecfd501f
0101oak <115372729+0101oak@users.noreply.github.com>
2024-12-16 01:35:31 +0300

    support page added (#155)
    * support page added
    
    * copy-form component deleted && support page is refactored
    
    * support page form refactored

---
+++ src/app/(content)/support/page.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+import { Button } from "@/components/ui/button";
+import { Form } from "@/components/ui/form";
+import InputField from "@/components/ui/form/fields/input-field";
+import { CopyIcon } from "@/components/ui/icons/copy-icon";
+import { Text } from "@/components/ui/text";
+
+import { defaultValues, SupportData, supportSchema } from "./schema";
+
+export default function SupportPage() {
+  const form = useForm<SupportData>({
+    resolver: zodResolver(supportSchema),
+    defaultValues,
+  });
+
+  const handleCopyForm = () => {
+    const values = form.getValues();
+    const data = `full name: ${values.name}\norder #: ${values.orderNumber}\ntracking #: ${values.trackingNumber}\nrequest: ${values.request}`;
+    navigator.clipboard.writeText(data);
+  };
+
+  return (
+    <div className="mx-auto mt-16 w-full space-y-16 sm:w-2/5">
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(handleCopyForm)}
+          className="space-y-8"
+        >
+          <Text variant="uppercase">1/2 fill out the form</Text>
+          <InputField
+            label="full name:"
+            name="name"
+            placeholder="enter your name"
+            control={form.control}
+          />
+          <InputField
+            label="order #:"
+            name="orderNumber"
+            placeholder="enter your order number"
+            control={form.control}
+          />
+          <InputField
+            label="tracking #:"
+            name="trackingNumber"
+            placeholder="enter your tracking number"
+            control={form.control}
+          />
+          <InputField
+            label="your request:"
+            name="request"
+            placeholder="enter your request"
+            control={form.control}
+          />
+          <Button
+            variant="underline"
+            className="flex items-center gap-3 uppercase"
+          >
+            copy form <CopyIcon />
+          </Button>
+        </form>
+      </Form>
+      <div className="space-y-8">
+        <Text variant="uppercase">2/2 send us your completed form via</Text>
+        <div className="flex gap-4 ">
+          <Button variant="underlineWithColors">telegram bot</Button>
+          <Button variant="underlineWithColors">email</Button>
+        </div>
+      </div>
+    </div>
+  );
+}
