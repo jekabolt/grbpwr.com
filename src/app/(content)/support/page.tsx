b14a7f588b659a834330f44dd9365084e2008d30
oak <dubodeldanila@gmail.com>
2024-12-16 01:31:33 +0300

    support page form refactored

--- src/app/(content)/support/page.tsx
+++ src/app/(content)/support/page.tsx
@@ -4,8 +4,8 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 
 import { Button } from "@/components/ui/button";
+import { Form } from "@/components/ui/form";
 import InputField from "@/components/ui/form/fields/input-field";
-import { FormContainer } from "@/components/ui/form/form-container";
 import { CopyIcon } from "@/components/ui/icons/copy-icon";
 import { Text } from "@/components/ui/text";
 
@@ -25,45 +25,44 @@ export default function SupportPage() {
 
   return (
     <div className="mx-auto mt-16 w-full space-y-16 sm:w-2/5">
-      <FormContainer
-        form={form}
-        submitButton={
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(handleCopyForm)}
+          className="space-y-8"
+        >
+          <Text variant="uppercase">1/2 fill out the form</Text>
+          <InputField
+            label="full name:"
+            name="name"
+            placeholder="enter your name"
+            control={form.control}
+          />
+          <InputField
+            label="order #:"
+            name="orderNumber"
+            placeholder="enter your order number"
+            control={form.control}
+          />
+          <InputField
+            label="tracking #:"
+            name="trackingNumber"
+            placeholder="enter your tracking number"
+            control={form.control}
+          />
+          <InputField
+            label="your request:"
+            name="request"
+            placeholder="enter your request"
+            control={form.control}
+          />
           <Button
             variant="underline"
             className="flex items-center gap-3 uppercase"
           >
             copy form <CopyIcon />
           </Button>
-        }
-        onSubmit={handleCopyForm}
-        className="space-y-8"
-      >
-        <Text variant="uppercase">1/2 fill out the form</Text>
-        <InputField
-          label="full name:"
-          name="name"
-          placeholder="enter your name"
-          control={form.control}
-        />
-        <InputField
-          label="order #:"
-          name="orderNumber"
-          placeholder="enter your order number"
-          control={form.control}
-        />
-        <InputField
-          label="tracking #:"
-          name="trackingNumber"
-          placeholder="enter your tracking number"
-          control={form.control}
-        />
-        <InputField
-          label="your request:"
-          name="request"
-          placeholder="enter your request"
-          control={form.control}
-        />
-      </FormContainer>
+        </form>
+      </Form>
       <div className="space-y-8">
         <Text variant="uppercase">2/2 send us your completed form via</Text>
         <div className="flex gap-4 ">
