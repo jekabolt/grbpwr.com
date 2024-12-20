a9a787a2516fadf5aeeec6ec37715f40f300788b
0101oak <115372729+0101oak@users.noreply.github.com>
2024-12-20 16:40:27 +0300

    shopping cart page created && categories thumb moved into icons folder (#158)
    * shopping cart page created && categories thumb moved into icons folder
    
    * cart total price div container styles changed

--- src/app/(checkout)/cart/_components/CartTotalPrice.tsx
+++ src/app/(checkout)/cart/_components/CartTotalPrice.tsx
@@ -2,25 +2,18 @@
 
 import { useCart } from "@/lib/stores/cart/store-provider";
 import { useDataContext } from "@/components/DataContext";
+import { Text } from "@/components/ui/text";
 
 export default function CartTotalPrice() {
   const { selectedCurrency } = useDataContext();
-  const { totalPrice, subTotalPrice } = useCart((state) => state);
+  const { subTotalPrice } = useCart((state) => state);
 
   return (
-    <div className="border-t border-dashed border-textColor pt-6 text-textColor">
-      <div className="flex justify-between">
-        <span>TOTAL:</span>
-        <span>
-          {totalPrice} {selectedCurrency}
-        </span>
-      </div>
-      <div className="flex justify-between">
-        <span>SUBTOTAL:</span>
-        <span>
-          {subTotalPrice} {selectedCurrency}
-        </span>
-      </div>
+    <div className="space-y-6">
+      <Text variant="uppercase">subtotal:</Text>
+      <Text variant="uppercase">
+        {subTotalPrice} {selectedCurrency}
+      </Text>
     </div>
   );
 }
