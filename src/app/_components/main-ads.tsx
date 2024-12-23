94b530b9fb2101fc40207e4710d01e6882171066
oak <dubodeldanila@gmail.com>
2024-12-23 22:39:17 +0300

    main page styles refactored

--- src/app/_components/main-ads.tsx
+++ src/app/_components/main-ads.tsx
@@ -1,20 +1,34 @@
 import { common_HeroMainAdd } from "@/api/proto-http/frontend";
 
 import Image from "@/components/ui/image";
+import { Text } from "@/components/ui/text";
 
 export function MainAds({ main }: { main?: common_HeroMainAdd }) {
   if (!main) return null;
 
   return (
-    <div className="flex h-screen items-center justify-center bg-slate-50">
-      <div className="h-[600px]">
+    <div className="relative -mt-20 h-full w-full ">
+      <div className="h-[800px]">
         <Image
-          fit="contain"
           src={main.singleAdd?.media?.media?.fullSize?.mediaUrl!}
           alt="main hero image"
           aspectRatio="4/3"
           // blurHash={media.media?.blurhash}
         />
+      </div>
+      <div className="absolute inset-0 z-10 flex items-center">
+        <div className="flex w-3/4 items-start justify-between ">
+          <Text variant="uppercase" className="text-white">
+            collection
+          </Text>
+          <Text variant="uppercase" className="text-white">
+            Light_leather_organza
+          </Text>
+          <Text variant="uppercase" className="text-white">
+            Actor Ethan Ruan is featured in the <br /> next chapter of Community
+            as a <br /> Form of Research wearing
+          </Text>
+        </div>
       </div>
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,16 @@ import { serviceClient } from "@/lib/api";
 import NavigationLayout from "@/components/navigation-layout";
 import { Button } from "@/components/ui/button";
 import { Ads } from "@/app/_components/ads";
-import { MainAds } from "@/app/_components/main-ads";
+
+import { MainAds } from "./_components/main-ads";
 
 export default async function Page() {
   const { hero } = await serviceClient.GetHero({});
 
   return (
     <>
-      {/* <MainAds main={hero?.entities?.[0]?.mainAdd} /> */}
       <NavigationLayout>
+        <MainAds main={hero?.entities?.[0]?.mainAdd} />
         <Ads entities={hero?.entities || []} />
         <Button asChild size="giant" variant="simple">
           <Link href="/catalog">view all</Link>
