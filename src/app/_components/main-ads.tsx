39a0ab1b695c6be0b0019a959a43604313947c67
oak <dubodeldanila@gmail.com>
2024-12-27 20:17:55 +0300

    featured product section refactored && double add section refactored

--- src/app/_components/main-ads.tsx
+++ src/app/_components/main-ads.tsx
@@ -9,7 +9,7 @@ export function MainAds({ main }: { main?: common_HeroMain }) {
   if (!main) return null;
 
   return (
-    <div className="h-screen w-full">
+    <div className="h-[calc(100vh-16px)] w-full lg:h-[calc(100vh-56px)]">
       <div className="absolute inset-0 z-0 h-screen">
         <Image
           src={main.single?.media?.media?.fullSize?.mediaUrl!}
@@ -22,22 +22,15 @@ export function MainAds({ main }: { main?: common_HeroMain }) {
       <div className="absolute inset-0 z-10 flex h-screen items-center">
         <div className="flex flex-col items-start gap-6 p-2 md:flex-row md:justify-between ">
           <Text variant="uppercase" className="text-white">
-            collection
-            {/* now doesn't work*/}
-            {/* {main.tag} */}
+            {main.tag}
           </Text>
           <Text variant="uppercase" className="text-white">
-            Light_leather_organza
-            {/* now doesn't work*/}
-            {/* {main.single?.headline} */}
+            {main.single?.headline}
           </Text>
           <Text variant="uppercase" className="text-white md:w-1/3">
-            Actor Ethan Ruan is featured in the next chapter of Community as a
-            Form of Research wearing
-            {/* now doesn't work*/}
-            {/* {main.description} */}
+            {main.description}
           </Text>
-          <Button asChild variant="underline" className="text-white">
+          <Button variant="underline" className="uppercase text-white" asChild>
             <Link href={main.single?.exploreLink || ""}>
               {main.single?.exploreText}
             </Link>
