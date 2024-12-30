72e00a2ce6a09d037f9a23d77533a70693ef52c3
oak <dubodeldanila@gmail.com>
2024-12-30 15:16:50 +0300

    padding added for footer

--- src/components/navigation-layout.tsx
+++ src/components/navigation-layout.tsx
@@ -12,7 +12,7 @@ export default function NavigationLayout({
     <div className="relative min-h-screen bg-bgColor">
       <Header />
       <div className="w-full space-y-20 md:px-0">{children}</div>
-      <Footer hideForm={hideForm} className="p-2 pb-14 md:p-0" />
+      <Footer hideForm={hideForm} className="p-2 pb-14 md:pb-0" />
     </div>
   );
 }
