94a433b3b193294abd24ea5373f7673bf0ed16bb
oak <dubodeldanila@gmail.com>
2025-01-15 15:30:37 +0300

    styles for sizes filter refactored; category popover removed from catalog filters

--- src/app/catalog/_components/Category.tsx
+++ src/app/catalog/_components/Category.tsx
@@ -1,45 +1,14 @@
 "use client";
 
 import { getCategoryName } from "@/lib/categories-map";
-import { useDataContext } from "@/components/DataContext";
-import GenericPopover from "@/components/ui/popover";
 import { Text } from "@/components/ui/text";
 
-import FilterOptionButtons from "./FilterOptionButtons";
 import useFilterQueryParams from "./useFilterQueryParams";
 
-function Trigger({ defaultValue }: { defaultValue: string }) {
-  return <Text variant="uppercase">{defaultValue}</Text>;
-}
-
 export default function Category() {
-  const { dictionary } = useDataContext();
-  const {
-    defaultValue: defaultCategory,
-    handleFilterChange: handleCategoryChange,
-  } = useFilterQueryParams("category");
+  const { defaultValue: defaultCategory } = useFilterQueryParams("category");
 
   return (
-    <GenericPopover
-      contentProps={{
-        side: "bottom",
-        align: "start",
-      }}
-      title="categories"
-      openElement={
-        <Trigger
-          defaultValue={`${getCategoryName(defaultCategory) || "all categories"}`}
-        />
-      }
-    >
-      <div className="mb-8">
-        <FilterOptionButtons
-          defaultOptionText="all"
-          defaultValue={defaultCategory || ""}
-          handleFilterChange={handleCategoryChange}
-          values={dictionary?.categories || []}
-        />
-      </div>
-    </GenericPopover>
+    <Text variant="uppercase">{`${getCategoryName(defaultCategory) || "all categories"}`}</Text>
   );
 }
