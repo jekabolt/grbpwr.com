40658b44b1b2c272110b8b9fd7d4fea653568fce
oak <dubodeldanila@gmail.com>
2025-01-16 13:52:40 +0300

    refactor descktop navbar

--- src/components/ui/desktop-nav-menu.tsx
+++ src/components/ui/desktop-nav-menu.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
+import { GENDER_MAP } from "@/constants";
 import * as NavigationMenu from "@radix-ui/react-navigation-menu";
 
 import { groupCategories } from "@/lib/categories-map";
@@ -7,33 +8,29 @@ import { cn } from "@/lib/utils";
 import { useDataContext } from "@/components/DataContext";
 
 import { Button } from "./button";
+import { Text } from "./text";
 
 export function DesktopNavigationMenu({ className }: { className?: string }) {
   const { dictionary } = useDataContext();
-
+  const men = `gender=${GENDER_MAP["men"]}`;
+  const women = `gender=${GENDER_MAP["women"]}`;
   const categoriesGroups = groupCategories(
     dictionary?.categories?.map((v) => v.name as string) || [],
   );
 
   return (
     <NavigationMenu.Root className={cn("flex-none", className)}>
-      <NavigationMenu.List className="flex list-none gap-5">
+      <NavigationMenu.List className="flex list-none gap-1">
         <NavigationMenu.Item>
           <NavigationMenu.Trigger
             className={cn(
-              "bg-textColor text-bgColor data-[state=open]:bg-bgColor data-[state=open]:text-textColor",
+              "bg-textColor px-2 text-bgColor data-[state=open]:bg-bgColor data-[state=open]:text-textColor",
             )}
           >
-            <Link href="/catalog">catalog</Link>
+            <Link href={`/catalog?${men}`}>men</Link>
           </NavigationMenu.Trigger>
-          <NavigationMenu.Content className="absolute left-0 top-0 bg-textColor p-5">
-            <div className="flex gap-x-8">
-              <div className="mr-16 space-y-4">
-                <Button variant={"underlineReverse"}>ALL</Button>
-                <Button variant={"underlineReverse"}>MEN</Button>
-                <Button variant={"underlineReverse"}>WOMEN</Button>
-              </div>
-
+          <NavigationMenu.Content className="blackTheme absolute left-0 top-0 w-full bg-bgColor p-5 text-textColor">
+            <div className="flex gap-x-7">
               {Object.entries(categoriesGroups).map(([key, category], i) => (
                 <LinksGroup
                   groupIndex={i}
@@ -42,7 +39,34 @@ export function DesktopNavigationMenu({ className }: { className?: string }) {
                   title={category.title}
                   links={category.items.map((item) => ({
                     title: item.label.toLowerCase(),
-                    href: item.href,
+                    href: `${item.href}&${men}`,
+                    id: item.id,
+                  }))}
+                />
+              ))}
+            </div>
+          </NavigationMenu.Content>
+        </NavigationMenu.Item>
+
+        <NavigationMenu.Item>
+          <NavigationMenu.Trigger
+            className={cn(
+              "bg-textColor px-2 text-bgColor data-[state=open]:bg-bgColor data-[state=open]:text-textColor",
+            )}
+          >
+            <Link href={`/catalog?${women}`}>women</Link>
+          </NavigationMenu.Trigger>
+          <NavigationMenu.Content className="blackTheme absolute left-0 top-0 w-full bg-bgColor p-5 text-textColor">
+            <div className="flex gap-x-7">
+              {Object.entries(categoriesGroups).map(([key, category], i) => (
+                <LinksGroup
+                  groupIndex={i}
+                  className="w-40"
+                  key={key}
+                  title={category.title}
+                  links={category.items.map((item) => ({
+                    title: item.label.toLowerCase(),
+                    href: `${item.href}&${women}`,
                     id: item.id,
                   }))}
                 />
@@ -53,18 +77,15 @@ export function DesktopNavigationMenu({ className }: { className?: string }) {
 
         <NavigationMenu.Item>
           <Button variant="simple" asChild>
-            <NavigationMenu.Link href="/catalog">ss{"'"}25</NavigationMenu.Link>
-          </Button>
-        </NavigationMenu.Item>
-        <NavigationMenu.Item>
-          <Button variant="simple" asChild>
-            <NavigationMenu.Link href="/archive">archive</NavigationMenu.Link>
+            <NavigationMenu.Link href="/archive" className="px-2">
+              archive
+            </NavigationMenu.Link>
           </Button>
         </NavigationMenu.Item>
       </NavigationMenu.List>
 
       <div className="perspective-[2000px] absolute left-0 top-full flex w-full justify-center">
-        <NavigationMenu.Viewport className="h-[var(--radix-navigation-menu-viewport-height)] w-full bg-textColor" />
+        <NavigationMenu.Viewport className="h-[var(--radix-navigation-menu-viewport-height)] w-full" />
       </div>
     </NavigationMenu.Root>
   );
@@ -89,17 +110,19 @@ function LinksGroup({
 
   return (
     <div className={cn("space-y-4", className)}>
-      <p className="uppercase text-bgColor">{title}</p>
+      <Text variant="uppercase" className="text-xl">
+        {title}
+      </Text>
       <div className="space-y-2">
         <div className="w-full">
-          <Button variant="simple" asChild>
+          <Button variant="simpleReverse" asChild>
             <NavigationMenu.Link href="/catalog">view all</NavigationMenu.Link>
           </Button>
         </div>
         {links.map((link) => (
           <div className="w-full" key={link.href}>
             <Button
-              variant={category === link.id ? "simpleReverse" : "simple"}
+              variant={category === link.id ? "simple" : "simpleReverse"}
               asChild
             >
               <NavigationMenu.Link href={link.href}>
