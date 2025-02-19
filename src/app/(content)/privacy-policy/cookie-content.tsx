327905dcfa8a0b1e02fdefc50f779a8c59074910
oak <dubodeldanila@gmail.com>
2025-02-19 19:11:25 +0300

    cookie banner refactored, cookie modal created

---
+++ src/app/(content)/privacy-policy/cookie-content.tsx
@@ -0,0 +1,89 @@
+import { Text } from "@/components/ui/text";
+import { ToggleSwitch } from "@/components/ui/toggle-switch";
+
+interface CookiePreferences {
+  functional: boolean;
+  statistical: boolean;
+  advertising_social_media: boolean;
+  experience: boolean;
+}
+
+interface Props {
+  preferences?: CookiePreferences;
+  onPreferenceChange?: (key: string, value: boolean) => void;
+}
+
+export function CookieContent({
+  preferences,
+  onPreferenceChange = () => {},
+}: Props) {
+  return (
+    <div className="space-y-4">
+      <Text className="lowercase">
+        A cookie is a small text file that is stored in a dedicated location on
+        your computer, tablet, smartphone or other device when you use your
+        browser to visit an online service. A cookie allows its sender to
+        identify the device on which it is stored during the period of validity
+        of consent, which does not exceed 13 months.
+      </Text>
+
+      <ToggleSwitch label="functional cookie (non-optional)" disabled checked />
+
+      <Text className="lowercase">
+        These cookies are required for optimum operation of the website, and
+        cannot be configured. They allow us to offer you the key functions of
+        the website (language used, display resolution, account access, shopping
+        bag, wish list, etc.), provide you with online advice and secure our
+        website against any attempted fraud.
+      </Text>
+
+      <ToggleSwitch
+        label="statistical analysis cookies"
+        checked={preferences?.statistical}
+        onCheckedChange={(checked: boolean) =>
+          onPreferenceChange("statistical", checked)
+        }
+      />
+
+      <Text className="lowercase">
+        These cookies are used to measure and analyse our website audience
+        (visitor volume, pages viewed, average browsing time, etc.) to help us
+        improve its performance.By accepting these cookies, you are helping us
+        to improve our website.
+      </Text>
+
+      <ToggleSwitch
+        label="advertising and social media cookies"
+        checked={preferences?.advertising_social_media}
+        onCheckedChange={(checked: boolean) =>
+          onPreferenceChange("advertising_social_media", checked)
+        }
+      />
+
+      <Text className="lowercase">
+        These cookies are used for Off White advertisements displayed on
+        third-party websites, including social media, and are tailored to your
+        preferences and to help us measure the effectiveness of our advertising
+        campaigns. If you deactivate these cookies,advertising (including ours)
+        will continue to be displayed as you browse the Internet, although they
+        will not be specific to your personal interests, and will therefore be
+        less relevant.
+      </Text>
+
+      <ToggleSwitch
+        label="cookies to personalize the grbpwr experience"
+        checked={preferences?.experience}
+        onCheckedChange={(checked: boolean) =>
+          onPreferenceChange("experience", checked)
+        }
+      />
+
+      <Text className="lowercase">
+        These cookies allow us to provide you with online or in-store
+        recommendations of products, services and content that match your
+        expectations and preferences. By accepting these cookies, you are opting
+        for an enriched and personalised experience.
+      </Text>
+    </div>
+  );
+}
---
+++ src/components/ui/toggle-switch.tsx
@@ -0,0 +1,35 @@
+import { Label } from "@radix-ui/react-label";
+import * as Switch from "@radix-ui/react-switch";
+
+import { Text } from "./text";
+
+export function ToggleSwitch({
+  checked = false,
+  label,
+  disabled,
+  onCheckedChange,
+}: {
+  checked?: boolean;
+  label?: string;
+  disabled?: boolean;
+  onCheckedChange?: (checked: boolean) => void;
+}) {
+  return (
+    <div className="flex items-center gap-5">
+      <Switch.Root
+        id="toggle"
+        checked={checked}
+        disabled={disabled}
+        onCheckedChange={onCheckedChange}
+        className="relative h-3 w-6 cursor-pointer border border-textColor bg-bgColor outline-none disabled:opacity-50 data-[state=checked]:bg-textColor "
+      >
+        <Switch.Thumb className="block h-2 w-2 translate-x-0.5 bg-textColor transition-transform duration-100 will-change-transform data-[state=checked]:translate-x-[13px] data-[state=checked]:bg-white" />
+      </Switch.Root>
+      {label && (
+        <Label htmlFor="toggle" className={disabled ? "opacity-50" : ""}>
+          <Text variant="uppercase">{label}</Text>
+        </Label>
+      )}
+    </div>
+  );
+}
