c4aa64d749f5b6134d74b5bb8634c47c92573ca4
oak <dubodeldanila@gmail.com>
2025-02-20 00:08:49 +0300

    wip

--- src/components/ui/mobile-cookie-modal.tsx
+++ src/components/ui/mobile-cookie-modal.tsx
@@ -56,6 +56,9 @@ export function MobileCookieModal() {
       <DialogPrimitives.Portal>
         <DialogPrimitives.Overlay className="fixed inset-0 z-10 bg-overlay" />
         <DialogPrimitives.Content className="fixed inset-0 z-30 flex flex-col gap-4 bg-bgColor py-4">
+          <DialogPrimitives.Title className="sr-only">
+            grbpwr mobile menu
+          </DialogPrimitives.Title>
           <div className="flex items-center justify-between px-5">
             <Text variant="uppercase">Cookie preferences</Text>
             <DialogPrimitives.Close asChild>
