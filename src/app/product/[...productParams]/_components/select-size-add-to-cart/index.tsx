9437b110ca0b582a6d9765ad3fcb018a54ab0d0e
oak <dubodeldanila@gmail.com>
2025-03-11 02:28:01 +0300

    disable out of stock sizes

--- src/app/product/[...productParams]/_components/select-size-add-to-cart/index.tsx
+++ src/app/product/[...productParams]/_components/select-size-add-to-cart/index.tsx
@@ -14,6 +14,7 @@ import { Button } from "@/components/ui/button";
 import { Text } from "@/components/ui/text";
 
 import { useData } from "./useData";
+import { useDisabled } from "./useDisabled";
 import { useHandlers } from "./useHandlers";
 
 export function AddToCartForm({
@@ -48,6 +49,10 @@ export function AddToCartForm({
     activeSizeId,
   });
 
+  const { outOfStock } = useDisabled({ id, activeSizeId, product });
+
+  console.log(outOfStock);
+
   useEffect(() => {
     if (isOneSize && sizeNames) {
       handleSizeSelect(sizeNames[0].id);
@@ -74,11 +79,13 @@ export function AddToCartForm({
             <AccordionContent className="grid grid-cols-4 gap-2">
               {sizeNames?.map(({ name, id }) => (
                 <Button
-                  className={cn("uppercase hover:border-b hover:border-black", {
+                  className={cn("uppercase", {
                     "border-b border-black": activeSizeId === id,
+                    "hover:border-b hover:border-black": !outOfStock[id],
                   })}
                   key={id}
                   onClick={() => handleSizeSelect(id)}
+                  disabled={outOfStock[id]}
                 >
                   {name}
                 </Button>
--- src/app/product/[...productParams]/_components/select-size-add-to-cart/useDisabled.tsx
+++ src/app/product/[...productParams]/_components/select-size-add-to-cart/useDisabled.tsx
@@ -1,23 +1,43 @@
+import { common_ProductFull } from "@/api/proto-http/frontend";
+
 import { useCart } from "@/lib/stores/cart/store-provider";
 import { useDataContext } from "@/components/DataContext";
 
 type Props = {
   id: number;
+  product: common_ProductFull;
   activeSizeId: number | undefined;
 };
 
-export function useDisabled({ id, activeSizeId }: Props) {
+export function useDisabled({ id, activeSizeId, product }: Props) {
   const { dictionary } = useDataContext();
   const { products } = useCart((state) => state);
 
   const productQuantityInCart =
-    products.find((p) => p.id === id && p.size === activeSizeId?.toString())
-      ?.quantity || 0;
+    products.find(
+      (p) => p && p.id === id && p.size === activeSizeId?.toString(),
+    )?.quantity || 0;
 
   const maxOrderItems = dictionary?.maxOrderItems || 3;
   const isMaxQuantity = productQuantityInCart >= maxOrderItems;
 
+  const activeSize = product?.sizes?.find(
+    (size) => size.sizeId === activeSizeId,
+  );
+  const activeOutOfStock = activeSize?.quantity?.value === "0";
+
+  // Create a map of sizeId to outOfStock status
+  const outOfStock =
+    product?.sizes?.reduce(
+      (acc, size) => {
+        acc[size.sizeId || 0] = size.quantity?.value === "0";
+        return acc;
+      },
+      {} as Record<number, boolean>,
+    ) || {};
+
   return {
     isMaxQuantity,
+    outOfStock,
   };
 }
