71eb4d08dc9f2da33957919a71543a65eb065cdd
oak <dubodeldanila@gmail.com>
2025-03-16 01:01:23 +0300

    fix loader

--- src/components/ui/loader.tsx
+++ src/components/ui/loader.tsx
@@ -1,8 +1,8 @@
 export function Loader() {
   return (
-    <div className="flex items-center justify-center p-2">
-      <div className="h-[0.5px] w-[173px] bg-gray-200/20">
-        <div className="h-full origin-left animate-[loading_0.5s_ease-out_forwards] bg-textColor" />
+    <div className="flex w-full justify-center p-2">
+      <div className="relative h-[0.5px] w-[175px] overflow-hidden bg-gray-200/20">
+        <div className="absolute left-0 top-0 h-full w-full animate-[loading_1s_ease-out_forwards] bg-textColor" />
       </div>
     </div>
   );
