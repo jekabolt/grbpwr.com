5eb72c27cce1a6205a81727204febc9d16f47a6c
oak <dubodeldanila@gmail.com>
2025-03-17 21:46:59 +0300

    add metadata for main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,38 @@
+import { Metadata } from "next";
+
 import { serviceClient } from "@/lib/api";
 import FlexibleLayout from "@/components/flexible-layout";
 import { EmptyHero } from "@/components/ui/empty-hero";
 import { Ads } from "@/app/_components/ads";
 
 import { MainAds } from "./_components/main-ads";
+
+export async function generateMetadata(): Promise<Metadata> {
+  try {
+    const { hero } = await serviceClient.GetHero({});
+    const main =
+      hero?.entities?.[0].main?.single?.mediaLandscape?.media?.fullSize
+        ?.mediaUrl;
+    const single =
+      hero?.entities?.[0].single?.mediaLandscape?.media?.fullSize?.mediaUrl;
+    const double =
+      hero?.entities?.[0].double?.left?.mediaLandscape?.media?.fullSize
+        ?.mediaUrl;
+
+    return {
+      title: "main",
+      description: "main",
+      openGraph: {
+        images: main,
+      },
+    };
+  } catch (error) {
+    return {
+      title: "My Page Title",
+      description: "My Page Description",
+    };
+  }
+}
 
 export default async function Page() {
   const { hero } = await serviceClient.GetHero({});
