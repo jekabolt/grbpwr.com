2573e21ab17e6868bdf4949b960bf454eaf866af
oak <dubodeldanila@gmail.com>
2025-04-22 16:08:04 +0300

    not found page fixed

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,33 +1,29 @@
+import "./globals.css";
+
 import Link from "next/link";
 
+import FlexibleLayout from "@/components/flexible-layout";
 import { Button } from "@/components/ui/button";
 import { Text } from "@/components/ui/text";
-import { Header } from "@/app/_components/header";
-
-import "./globals.css";
 
 export const dynamic = "force-static";
 
 export default function NotFoundPage() {
   return (
-    <div className="relative">
-      <Header />
-      <div className="space-y-6 px-2.5 pt-6 lg:px-24 lg:pt-0">
-        <Text component="h1">page not found</Text>
-        <Text className="lg:max-w-xl" size="small">
+    <FlexibleLayout headerType="catalog" footerType="regular" transparent>
+      <div className="flex h-screen flex-col items-center justify-center gap-6">
+        <Text variant="uppercase" component="h1">
+          page not found
+        </Text>
+        <Text className="max-w-xs px-2.5 text-center lg:max-w-72 lg:px-0">
           sorry, the page you are looking for doesn&apos;t exist or has been
           moved. please go back to the homepage or contact us if the problem
           persists.
         </Text>
-        <div className="flex gap-x-20">
-          <Button asChild variant="underlineWithColors">
-            <Link href="/about">contact us</Link>
-          </Button>
-          <Button variant="main" size="lg" asChild>
-            <Link href="/catalog">catalog</Link>
-          </Button>
-        </div>
+        <Button asChild variant="main" size="lg" className="uppercase">
+          <Link href="/">main</Link>
+        </Button>
       </div>
-    </div>
+    </FlexibleLayout>
   );
 }
