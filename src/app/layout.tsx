cfd658e28fdb0754a466c5b2540432d80c6b6524
oak <dubodeldanila@gmail.com>
2025-04-23 15:20:27 +0300

    wip

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,9 +9,12 @@ export const metadata: Metadata = {
   title: "grbpwr.com title",
   description: "grbpwr.com desc",
   openGraph: {
-    title: "GRBPWR",
+    title: "grbpwr.com title",
     description:
       "Discover the latest ready-to-wear GRBPWR menswear, womenswear, and accessories at the GRBPWR online store. Worldwide express shipping.",
+    type: "website",
+    url: "https://grbpwr.com",
+    siteName: "grbpwr.com",
   },
 };
 
