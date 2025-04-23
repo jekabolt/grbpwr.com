81536571dafd637b3a3ebc7b81e6f15855753136
oak <dubodeldanila@gmail.com>
2025-04-23 14:26:56 +0300

    add dynamic metadata to fetch hero main add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,24 @@
+import { Metadata } from "next";
+
 import { serviceClient } from "@/lib/api";
 import FlexibleLayout from "@/components/flexible-layout";
 import { EmptyHero } from "@/components/ui/empty-hero";
 import { Ads } from "@/app/_components/ads";
 
 import { MainAds } from "./_components/main-ads";
+
+export async function generateMetadata(): Promise<Metadata> {
+  const { hero } = await serviceClient.GetHero({});
+  const heroImage =
+    hero?.entities?.[0]?.main?.single?.mediaLandscape?.media?.fullSize
+      ?.mediaUrl || "";
+
+  return {
+    openGraph: {
+      images: [heroImage],
+    },
+  };
+}
 
 export default async function Page() {
   const { hero } = await serviceClient.GetHero({});
