bffd95d5eac36d35868ba05da75d2b609182a456
oak <dubodeldanila@gmail.com>
2025-05-12 15:48:15 +0300

    wip

--- src/app/(content)/layout.tsx
+++ src/app/(content)/layout.tsx
@@ -8,7 +8,7 @@ export default function Layout({
   children: React.ReactNode;
 }>) {
   return (
-    <FlexibleLayout headerType="flexible" footerType="mini">
+    <FlexibleLayout headerType="catalog" footerType="mini">
       <div className="px-4 text-sm md:text-base lg:px-32 lg:py-10 lg:text-lg">
         {children}
       </div>
