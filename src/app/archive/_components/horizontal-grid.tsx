bbc18951f2712fe8b52404b89952e0fd1f2e856b
oak <dubodeldanila@gmail.com>
2025-05-12 23:38:18 +0300

    wip

--- src/app/archive/_components/horizontal-grid.tsx
+++ src/app/archive/_components/horizontal-grid.tsx
@@ -4,7 +4,6 @@ import Link from "next/link";
 import type { common_ArchiveFull } from "@/api/proto-http/frontend";
 
 import { calculateAspectRatio } from "@/lib/utils";
-import { Button } from "@/components/ui/button";
 import ImageComponent from "@/components/ui/image";
 import { Text } from "@/components/ui/text";
 
@@ -17,23 +16,25 @@ export function HorizontalGrid({
     <div className="h-full px-2.5 pb-2.5 lg:px-7">
       <div className="grid grid-cols-3 gap-x-2 gap-y-6 lg:grid-cols-8 lg:gap-x-4 lg:gap-y-10">
         {archives.map((archive, index) => (
-          <Button asChild key={archive.id}>
-            <Link href={archive.slug || ""} className="group block space-y-2">
-              <div className="relative">
-                <ImageComponent
-                  alt={archive.heading + " " + index}
-                  src={archive.media?.[0].media?.fullSize?.mediaUrl || ""}
-                  aspectRatio={calculateAspectRatio(
-                    archive.media?.[0].media?.fullSize?.width,
-                    archive.media?.[0].media?.fullSize?.height,
-                  )}
-                />
-              </div>
-              <Text variant="inactive" className="group-hover:text-textColor">
-                {archive.heading}
-              </Text>
-            </Link>
-          </Button>
+          <Link
+            href={archive.slug || ""}
+            key={archive.id}
+            className="group block space-y-2"
+          >
+            <div className="relative">
+              <ImageComponent
+                alt={archive.heading + " " + index}
+                src={archive.media?.[0].media?.fullSize?.mediaUrl || ""}
+                aspectRatio={calculateAspectRatio(
+                  archive.media?.[0].media?.fullSize?.width,
+                  archive.media?.[0].media?.fullSize?.height,
+                )}
+              />
+            </div>
+            <Text variant="inactive" className="group-hover:text-textColor">
+              {archive.heading}
+            </Text>
+          </Link>
         ))}
       </div>
     </div>
