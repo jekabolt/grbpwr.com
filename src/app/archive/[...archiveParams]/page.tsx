cabfe3f07d36fce605364fbc384a7bf94d07babd
0101oak <115372729+0101oak@users.noreply.github.com>
2025-05-21 18:31:38 +0300

    next archive (#222)
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip

---
+++ src/app/archive/[...archiveParams]/page.tsx
@@ -0,0 +1,92 @@
+import { Metadata } from "next";
+import { notFound } from "next/dist/client/components/not-found";
+import { common_ArchiveFull } from "@/api/proto-http/frontend";
+
+import { serviceClient } from "@/lib/api";
+import { generateCommonMetadata } from "@/lib/common-metadata";
+import FlexibleLayout from "@/components/flexible-layout";
+
+import { FullSizeItem } from "../_components/full-size-item";
+import PageComponent from "./_components/page-component";
+
+interface ArchivePageParams {
+  params: Promise<{
+    archiveParams: string[];
+  }>;
+}
+
+export async function generateMetadata({
+  params,
+}: ArchivePageParams): Promise<Metadata> {
+  const { archiveParams } = await params;
+
+  const [heading, tag, id] = archiveParams;
+
+  const archiveResponse = await serviceClient.GetArchive({
+    heading,
+    tag,
+    id: parseInt(id),
+  });
+
+  const archive = archiveResponse.archive as common_ArchiveFull;
+
+  return generateCommonMetadata({
+    title: archive.heading?.toUpperCase() || "heading".toUpperCase(),
+    description: archive.description || "description",
+    ogParams: {
+      imageUrl: archive.media?.[0].media?.thumbnail?.mediaUrl || "",
+      imageAlt: archive.heading || "",
+    },
+  });
+}
+
+export const dynamic = "force-static";
+
+export default async function Page({ params }: ArchivePageParams) {
+  const { archiveParams } = await params;
+
+  let nextArchive: common_ArchiveFull | undefined;
+
+  if (archiveParams.length !== 3) {
+    notFound();
+  }
+
+  const [heading, tag, id] = archiveParams;
+  const { archive } = await serviceClient.GetArchive({
+    heading,
+    tag,
+    id: parseInt(id),
+  });
+
+  if (archive?.nextSlug) {
+    const parts = archive.nextSlug.split("/");
+    const nextParams = parts.slice(2);
+
+    const [nextHeading, nextTag, nextId] = nextParams;
+
+    const { archive: nextArchivee } = await serviceClient.GetArchive({
+      heading: nextHeading,
+      tag: nextTag,
+      id: parseInt(nextId),
+    });
+
+    nextArchive = nextArchivee;
+  }
+
+  return (
+    <FlexibleLayout
+      headerType="archive"
+      headerProps={{
+        left: "grbpwr.com",
+        center: "archive",
+      }}
+      footerType="mini"
+      theme="dark"
+      className="pt-16"
+    >
+      <PageComponent archive={archive} />
+
+      <FullSizeItem archive={nextArchive} className="w-full lg:w-96" />
+    </FlexibleLayout>
+  );
+}
---
+++ src/app/archive/_components/full-size-item.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import { common_ArchiveFull } from "@/api/proto-http/frontend";
+
+import { calculateAspectRatio } from "@/lib/utils";
+import ImageComponent from "@/components/ui/image";
+import { Text } from "@/components/ui/text";
+
+interface Props {
+  className?: string;
+  archive?: common_ArchiveFull;
+}
+
+export function FullSizeItem({ className, archive }: Props) {
+  return (
+    <div className="flex h-full w-full flex-col items-center justify-between gap-4 lg:flex-row">
+      <Text className="w-60 text-center lg:text-left">{archive?.heading}</Text>
+      <div className={className}>
+        <Link href={archive?.slug || ""}>
+          <ImageComponent
+            src={archive?.media?.[0].media?.fullSize?.mediaUrl || ""}
+            alt={archive?.heading || ""}
+            aspectRatio={calculateAspectRatio(
+              archive?.media?.[0].media?.fullSize?.width,
+              archive?.media?.[0].media?.fullSize?.height,
+            )}
+          />
+        </Link>
+      </div>
+      <Text className="w-60 text-center lg:text-right">{archive?.tag}</Text>
+    </div>
+  );
+}
