17ee0278186c1f798e9f685fd6367c0699a290d4
oak <dubodeldanila@gmail.com>
2025-05-26 15:57:58 +0300

    wip

--- src/app/(checkout)/checkout/_components/new-order-form/error-messages.ts
+++ src/app/(checkout)/checkout/_components/new-order-form/error-messages.ts
@@ -3,16 +3,16 @@ export const errorMessages = {
         min: 'first name must contain at least 1 character',
         max: 'first name must contain at most 40 characters',
         regex: {
-            restriction: /^[\p{L} .'-]+$/u,
-            message: 'only latin letters, spaces, hyphens, apostrophes, and periods are allowed',
+            restriction: /^(?!.*  )[\p{L} .'-]+$/u,
+            message: 'only latin letters, spaces, hyphens, apostrophes, and periods are allowed; no consecutive spaces',
         }
     },
     lastName: {
         min: 'last name must contain at least 1 character',
         max: 'last name must contain at most 40 characters',
         regex: {
-            restriction: /^[\p{L} .'-]+$/u,
-            message: 'only latin letters, spaces, hyphens, apostrophes, and periods are allowed',
+            restriction: /^(?!.*  )[\p{L} .'-]+$/u,
+            message: 'only latin letters, spaces, hyphens, apostrophes, and periods are allowed; no consecutive spaces',
         }
     },
     phone: {
@@ -22,24 +22,24 @@ export const errorMessages = {
     city: {
         min: 'city must contain at least 2 characters',
         regex: {
-            restriction: /^[\p{L} .'-]+$/u,
-            message: 'only latin letters, spaces, hyphens, apostrophes, and periods are allowed',
+            restriction: /^(?!.*  )[\p{L} .'-]+$/u,
+            message: 'only latin letters, spaces, hyphens, apostrophes, and periods are allowed; no consecutive spaces',
         }
     },
     address: {
         min: 'address must contain at least 10 characters',
         max: 'address must contain at most 40 characters',
         regex: {
-            restriction: /^[\p{L}0-9 .'-]+$/u,
-            message: 'only letters, numbers, spaces, hyphens, apostrophes, and periods are allowed',
+            restriction: /^(?!.*  )[\p{L}0-9 .'-]+$/u,
+            message: 'only letters, numbers, spaces, hyphens, apostrophes, and periods are allowed; no consecutive spaces',
         }
     },
     postalCode: {
         min: 'postal code must contain at least 2 characters',
         max: 'postal code must contain at most 12 characters',
         regex: {
-            restriction: /^[A-Za-z0-9 \-]{2,12}$/,
-            message: 'postal code must contain only letters, numbers, spaces, or hyphens'
+            restriction: /^(?!.*  )[A-Za-z0-9 \- ]{2,12}$/,
+            message: 'postal code must contain only letters, numbers, spaces, or hyphens; no consecutive spaces'
         }
     },
     country: {
@@ -47,7 +47,11 @@ export const errorMessages = {
     },
     company: {
         min: 'company must contain at least 1 character',
-        max: 'company must contain at most 40 characters'
+        max: 'company must contain at most 40 characters',
+        regex: {
+            restriction: /^(?!.*  )[\p{L}0-9 .'-]+$/u,
+            message: 'only letters, numbers, spaces, hyphens, apostrophes, and periods are allowed; no consecutive spaces',
+        }
     },
     email: {
         invalid: 'invalid email',
