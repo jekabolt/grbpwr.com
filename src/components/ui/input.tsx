05e153bf30f3a5933132b85d6fca091b2fcdf319
oak <dubodeldanila@gmail.com>
2025-05-28 17:38:46 +0300

    select & input placeholder colors unified

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,32 +1,4 @@
-import { cva } from "class-variance-authority";
-
-export const variants = cva("w-full focus:outline-none focus:ring-0", {
-  variants: {
-    variant: {
-      default: [
-        "bg-bgColor",
-        "border-b",
-        "border-textColor",
-        "text-textColor",
-        "placeholder:text-textColor",
-      ],
-      secondary: [
-        "bg-bgColor",
-        "border-b",
-        "border-textColor",
-        "placeholder:text-textInactiveColor",
-        "placeholder-shown:border-textInactiveColor",
-      ],
-    },
-    size: {
-      default: ["text-textBaseSize"],
-    },
-  },
-  defaultVariants: {
-    variant: "default",
-    size: "default",
-  },
-});
+import { cn } from "@/lib/utils";
 
 export interface InputProps {
   type?: "email" | "number" | "tel" | "text";
@@ -35,20 +7,16 @@ export interface InputProps {
   [k: string]: any;
 }
 
-function Input({
-  type = "text",
-  className,
-  name,
-  ref,
-  variant,
-  ...props
-}: InputProps) {
+function Input({ type = "text", className, name, ref, ...props }: InputProps) {
   return (
     <input
       id={name}
       type={type}
       ref={ref}
-      className={variants({ variant, className })}
+      className={cn(
+        "w-full border-b border-textColor bg-bgColor text-textBaseSize focus:outline-none",
+        className,
+      )}
       {...props}
     />
   );
--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -89,7 +89,7 @@ export function SelectTrigger({
   return (
     <Select.Trigger
       className={cn(
-        "inline-flex w-full items-center justify-between gap-2 border-b border-b-textColor bg-bgColor text-textBaseSize focus:outline-none focus:ring-0 data-[placeholder]:border-textInactiveColor data-[placeholder]:text-textColor",
+        "inline-flex w-full items-center justify-between gap-2 border-b border-b-textColor bg-bgColor text-textBaseSize focus:outline-none focus:ring-0",
         className,
       )}
       aria-label={placeholder}
