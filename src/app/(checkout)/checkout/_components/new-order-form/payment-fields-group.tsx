f2a4b565a0252e6dbfe7d05c07f72e770d384ef3
oak <dubodeldanila@gmail.com>
2025-07-15 22:06:04 +0300

    billing address fixed

--- src/app/(checkout)/checkout/_components/new-order-form/payment-fields-group.tsx
+++ src/app/(checkout)/checkout/_components/new-order-form/payment-fields-group.tsx
@@ -30,10 +30,42 @@ export default function PaymentFieldsGroup({
   disabled = false,
 }: Props) {
   const { dictionary } = useDataContext();
-  const { watch } = useFormContext();
+  const { watch, setValue } = useFormContext();
 
   const billingAddressIsSameAsAddress = watch("billingAddressIsSameAsAddress");
   const paymentMethod = watch("paymentMethod");
+
+  const handleBillingCheckboxChange = (checked: boolean) => {
+    if (checked) {
+      const {
+        firstName,
+        lastName,
+        address,
+        additionalAddress,
+        city,
+        state,
+        country,
+        postalCode,
+        phone,
+        company,
+      } = watch();
+      const billingData = {
+        firstName,
+        lastName,
+        address,
+        additionalAddress,
+        city,
+        state,
+        country,
+        postalCode,
+        phone,
+        company,
+      };
+      Object.entries(billingData).forEach(([key, value]) =>
+        setValue(`billingAddress.${key}`, value),
+      );
+    }
+  };
 
   const allowedMethods =
     dictionary?.paymentMethods?.filter((v) => v.allowed) || [];
@@ -67,49 +99,6 @@ export default function PaymentFieldsGroup({
         items={paymentMethodsItems as any}
         disabled={disabled}
       />
-
-      {/*оплата картой делается на отдельной странице*/}
-
-      {/* {(paymentMethod === "PAYMENT_METHOD_NAME_ENUM_CARD" ||
-              paymentMethod === "PAYMENT_METHOD_NAME_ENUM_CARD_TEST") && (
-              <div>
-                <h1 className="text-9xl font-bold">stripe elements</h1>
-                <InputMaskedField
-                  control={form.control}
-                  name="creditCard.number"
-                  label="card number:"
-                  mask={"dddd dddd dddd dddd"}
-                  placeholder="4242 4242 4242 4242"
-                />
-                <InputField
-                  control={form.control}
-                  name="creditCard.fullName"
-                  label="name on card:"
-                  placeholder="James Bond"
-                />
-                <div className="grid grid-cols-2 gap-6">
-                  <div className="col-span-1">
-                    <InputMaskedField
-                      control={form.control}
-                      name="creditCard.expirationDate"
-                      label="expiration date:"
-                      mask={"__/__"}
-                      placeholder="MM/YY"
-                    />
-                  </div>
-                  <div className="col-span-1">
-                    <InputMaskedField
-                      control={form.control}
-                      name="creditCard.cvc"
-                      label="security code:"
-                      mask={"___"}
-                      placeholder="123"
-                    />
-                  </div>
-                </div>
-              </div>
-            )} */}
-
       <Text variant="uppercase" component="h2">
         billing address
       </Text>
@@ -122,6 +111,7 @@ export default function PaymentFieldsGroup({
           name="billingAddressIsSameAsAddress"
           label="same as shipping address"
           disabled={disabled}
+          onChange={handleBillingCheckboxChange}
         />
       </div>
 
