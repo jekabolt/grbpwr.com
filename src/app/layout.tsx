5ad25dc0d4086843027d13efabebac48b79dfac3
oak <dubodeldanila@gmail.com>
2025-07-23 16:19:17 +0300

    wip

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,12 +8,12 @@ import "./globals.css";
 
 export const metadata = generateCommonMetadata();
 
-export const viewport = {
-  width: "device-width",
-  initialScale: 1,
-  maximumScale: 1,
-  userScalable: false,
-};
+// export const viewport = {
+//   width: "device-width",
+//   initialScale: 1,
+//   maximumScale: 1,
+//   userScalable: false,
+// };
 
 export default function RootLayout({
   children,
