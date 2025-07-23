b28c97cb0044a48ed68339a5e9f702122b2d3c90
oak <dubodeldanila@gmail.com>
2025-07-23 15:55:15 +0300

    disable zoom

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,13 @@ import "./globals.css";
 
 export const metadata = generateCommonMetadata();
 
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+};
+
 type Props = {
   children: React.ReactNode;
 };
