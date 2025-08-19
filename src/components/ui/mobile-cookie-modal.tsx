fa128365c34a6963f17e0352a0730cf95ff6fd90
oak <dubodeldanila@gmail.com>
2025-08-19 14:49:47 +0300

    black theme for mobile cookie content

--- src/components/ui/mobile-cookie-modal.tsx
+++ src/components/ui/mobile-cookie-modal.tsx
@@ -48,7 +48,7 @@ export function MobileCookieModal({
       </div>
       <DialogPrimitives.Portal>
         <DialogPrimitives.Overlay className="fixed inset-0 z-10 bg-overlay" />
-        <DialogPrimitives.Content className="fixed inset-0 z-30 flex flex-col gap-4 bg-bgColor py-4">
+        <DialogPrimitives.Content className="blackTheme fixed inset-0 z-30 flex flex-col gap-4 bg-bgColor py-4 text-textColor">
           <DialogPrimitives.Title className="sr-only">
             grbpwr mobile menu
           </DialogPrimitives.Title>
