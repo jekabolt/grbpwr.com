569cf60fcff11940b8da7bd0f1b5f68b3498f0b8
oak <dubodeldanila@gmail.com>
2025-10-08 00:09:15 +0300

    begin checkout event added for the proceed to checkout btn

---
+++ src/lib/analitycs/checkout/index.ts
@@ -0,0 +1,32 @@
+import { common_OrderItem } from "@/api/proto-http/frontend";
+
+export function sendBeginCheckoutEvent(
+  currency: string,
+  products: common_OrderItem[],
+) {
+  window.dataLayer = window.dataLayer || [];
+  window.dataLayer.push({ ecommerce: null });
+
+  const totalValue = products.reduce((sum, p) => {
+    const price = parseFloat(p.productPriceWithSale || p.productPrice || "0");
+    const quantity = p.orderItem?.quantity || 1;
+    return sum + price * quantity;
+  }, 0);
+
+  window.dataLayer.push({
+    event: "begin_checkout",
+    ecommerce: {
+      currency: currency,
+      value: totalValue,
+      items: products.map((p) => ({
+        item_id: p.sku,
+        item_name: p.translations?.[0]?.name,
+        item_brand: p.productBrand,
+        item_variant: p.color,
+        discount: (parseFloat(p.productPrice || "0") * parseFloat(p.productSalePercentage || "0")) / 100,
+        price: p.productPriceWithSale,
+        quantity: p.orderItem?.quantity || 1,
+      })),
+    },
+  });
+}
