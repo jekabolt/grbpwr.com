616a03c3b55aa41dae238b7c45721c140a3ab4d7
oak <dubodeldanila@gmail.com>
2025-10-26 18:26:19 +0300

    wip

--- src/lib/analitycs/form/index.ts
+++ src/lib/analitycs/form/index.ts
@@ -1,5 +1,3 @@
-import { common_OrderFull } from "@/api/proto-http/frontend";
-import { calculateTotalValue } from "../utils";
 
 interface NewsletterFormData {
     email: string;
@@ -27,39 +25,4 @@ export function sendFormEvent(data: NewsletterFormData) {
     window.dataLayer.push(eventData);
 
     console.log('Newsletter subscription event sent to DataLayer:', eventData);
-}
-
-
-export function sendRefundEvent(data: common_OrderFull) {
-    if (typeof window === "undefined" || !data.order) return;
-
-    window.dataLayer = window.dataLayer || [];
-    window.dataLayer.push({ ecommerce: null });
-
-    window.dataLayer.push({
-        event: "refund",
-        ecommerce: {
-            currency: "EUR",
-            value: calculateTotalValue(data.orderItems || []),
-            transaction_id: data.order.uuid,
-            coupon: data.promoCode || "not set",
-            shipping: parseFloat(data.shipment?.cost?.value || "0") || 0,
-            items: data.orderItems?.map((i) => {
-                const price = parseFloat(i.productPrice || "0");
-                const salePercentage = parseFloat(i.productSalePercentage || "0");
-                const discount = (price * salePercentage) / 100;
-                return {
-                    item_id: i.sku || "",
-                    item_name: i.translations?.[0]?.name || "",
-                    affiliation: "GRBPWR STORE",
-                    coupon: data.promoCode || "not set",
-                    discount: discount,
-                    item_brand: i.productBrand,
-                    item_variant: i.orderItem?.sizeId?.toString() || "",
-                    price: price,
-                    quantity: 1
-                }
-            })
-        }
-    })
 }
