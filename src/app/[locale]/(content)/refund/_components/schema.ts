c29606966d2b67a90bfd7939c49e0f1141be5fd7
oak <dubodeldanila@gmail.com>
2025-12-15 18:30:54 +0300

    refund page refactor

--- src/app/[locale]/(content)/refund/_components/schema.ts
+++ src/app/[locale]/(content)/refund/_components/schema.ts
@@ -1,18 +1,20 @@
 import { errorMessages } from "@/constants";
 import { z } from "zod";
 
-
 export const refundForm = z.object({
-    email: z.string().max(40, errorMessages.email.max).email(errorMessages.email.invalid).trim(),
-    orderUuid: z.string(),
+    email: z
+        .string()
+        .max(40, errorMessages.email.max)
+        .email(errorMessages.email.invalid)
+        .trim(),
+    orderUuid: z.string().trim().min(1, "required"),
     reason: z.string(),
-})
-
+});
 
 export const defaultData = {
     email: "",
     orderUuid: "",
     reason: "other",
-}
+};
 
-export type RefundSchema = z.infer<typeof refundForm>
+export type RefundSchema = z.infer<typeof refundForm>;
