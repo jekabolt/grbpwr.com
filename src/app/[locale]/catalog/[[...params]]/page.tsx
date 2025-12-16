765e856b04f5484337c6fd52ea371223f8eb9b62
oak <dubodeldanila@gmail.com>
2025-12-16 18:11:28 +0300

    wip

--- src/app/[locale]/catalog/[[...params]]/page.tsx
+++ src/app/[locale]/catalog/[[...params]]/page.tsx
@@ -27,7 +27,7 @@ export async function generateStaticParams() {
   return [{ params: [] }, { params: ["men"] }, { params: ["women"] }];
 }
 
-// export const dynamic = "force-static";
+export const dynamic = "force-static";
 export const dynamicParams = true;
 
 export async function generateMetadata({
