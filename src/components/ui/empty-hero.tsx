22e479514eca89eb61d1424dff79b54ce9b57544
0101oak <115372729+0101oak@users.noreply.github.com>
2026-02-25 19:41:33 +0300

    translations for empty hero text (#443)

--- src/components/ui/empty-hero.tsx
+++ src/components/ui/empty-hero.tsx
@@ -1,14 +1,16 @@
 import Link from "next/link";
+import { useTranslations } from "next-intl";
 
 import FlexibleLayout from "../flexible-layout";
 import { Button } from "./button";
 
 export function EmptyHero() {
+  const t = useTranslations("empty-hero");
   return (
     <FlexibleLayout theme="dark">
       <div className="blackTheme absolute inset-0 flex h-screen w-full items-center justify-center bg-bgColor text-textColor">
         <Button variant="underline" className="uppercase" asChild>
-          <Link href="/catalog">shop now</Link>
+          <Link href="/catalog">{t("shop now")}</Link>
         </Button>
       </div>
     </FlexibleLayout>
