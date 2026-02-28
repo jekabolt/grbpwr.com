fad29107767c7795cbffeca3b3a6f4dbe4bc086c
oak <dubodeldanila@gmail.com>
2026-02-28 16:36:02 +0300

    fix analytics error

--- src/lib/analitycs/form/index.ts
+++ src/lib/analitycs/form/index.ts
@@ -11,6 +11,22 @@ interface NewsletterSignupData {
   page_path: string;
 }
 
+interface FormEventData {
+  email: string;
+  formId: string;
+}
+
+export function sendFormEvent(data: FormEventData) {
+  pushToDataLayer({
+    event: "form_submit",
+    ecommerce: {
+      currency: "EUR",
+      email: data.email,
+      form_id: data.formId,
+    },
+  });
+}
+
 export function sendGenerateLeadEvent(data: GenerateLeadData) {
   pushToDataLayer({
     event: "generate_lead",
@@ -26,6 +42,7 @@ export function sendNewsletterSignupEvent(data: NewsletterSignupData) {
   pushToDataLayer({
     event: "newsletter_signup",
     ecommerce: {
+      currency: "EUR",
       signup_location: data.signup_location,
       page_path: data.page_path,
     },
