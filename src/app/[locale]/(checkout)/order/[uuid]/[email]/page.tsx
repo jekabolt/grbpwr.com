386aea578b408a37e963abc5b47da291143ac61b
oak <dubodeldanila@gmail.com>
2026-04-06 15:38:23 +0300

    wip

--- src/app/[locale]/(checkout)/order/[uuid]/[email]/page.tsx
+++ src/app/[locale]/(checkout)/order/[uuid]/[email]/page.tsx
@@ -1,5 +1,4 @@
 import { notFound } from "next/navigation";
-
 import { ORDER_STATUS_DELIVERED_ID } from "@/constants";
 
 import { serviceClient } from "@/lib/api";
@@ -38,14 +37,12 @@ export default async function OrderPage(props: PageProps) {
   return (
     <FlexibleLayout>
       <div className="flex min-h-[calc(100dvh-10rem)] flex-col gap-10 px-2.5 pb-20 pt-24 lg:min-h-[calc(100dvh-12rem)] lg:px-32 lg:py-24">
-        <div className="flex min-h-0 flex-1 flex-col">
-          <OrderExperienceClient
-            isDelivered={isDelivered}
-            orderPromise={orderPromise}
-            orderUuid={uuid}
-            b64Email={email}
-          />
-        </div>
+        <OrderExperienceClient
+          isDelivered={isDelivered}
+          orderPromise={orderPromise}
+          orderUuid={uuid}
+          b64Email={email}
+        />
       </div>
     </FlexibleLayout>
   );
