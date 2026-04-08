77054cdc78f7f41eacb06df6c4fe7c9bc1e1e6ee
0101oak <115372729+0101oak@users.noreply.github.com>
2026-04-08 16:55:00 +0300

    wip (#509)

--- src/app/[locale]/_components/header.tsx
+++ src/app/[locale]/_components/header.tsx
@@ -50,15 +50,14 @@ export function Header({
       )}
       <header
         className={cn(
-          "fixed inset-x-2.5 top-2 z-30 h-12 py-2 lg:gap-0 lg:px-5 lg:py-3",
+          "fixed inset-x-2.5 z-30 h-12 py-2 lg:gap-0 lg:px-5 lg:py-3",
           "flex items-center justify-between gap-1",
           "border border-textInactiveColor bg-bgColor text-textColor lg:border-transparent",
-          "transition-transform duration-300 lg:translate-y-0",
-          "transform-gpu transition-transform duration-150 ease-[cubic-bezier(0.25,0.46,0.45,0.94)]",
-          "lg:transform-none lg:transition-[top] lg:duration-150 lg:ease-[cubic-bezier(0.25,0.46,0.45,0.94)]",
+          "transition-[top] duration-150 ease-[cubic-bezier(0.25,0.46,0.45,0.94)]",
           {
+            "top-2": isVisible,
+            "-top-16": !isVisible,
             "lg:top-8": open && showAnnounce && isAnnounceVisible,
-            "-translate-y-[calc(100%+0.5rem)]": !isVisible,
             "border-none bg-transparent text-bgColor mix-blend-exclusion":
               isAtTop && isMobile && theme !== "dark",
             "border-none bg-transparent text-textColor mix-blend-exclusion":
