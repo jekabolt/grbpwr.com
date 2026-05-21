f6ca1181047cbf2f50aec6ec99ca993795e1e9ba
oak <dubodeldanila@gmail.com>
2026-05-21 14:21:16 +0300

    change gap between text components inside the desktop product info card

--- src/app/[locale]/product/[...productParams]/_components/product-info.tsx
+++ src/app/[locale]/product/[...productParams]/_components/product-info.tsx
@@ -97,11 +97,10 @@ export function ProductInfo({ product }: { product: common_ProductFull }) {
         productName={name}
         productCategory={productCategory || ""}
       />
-      <div className="border-inactive absolute bottom-2.5 right-2.5 h-fit max-h-[566px] w-[300px] overflow-y-scroll border bg-bgColor p-2.5">
-        <div className="flex flex-col justify-between gap-16">
+      <div className="border-inactive absolute bottom-2.5 right-2.5 h-fit max-h-[564px] w-[300px] overflow-y-scroll border bg-bgColor p-2.5">
+        <div className="flex flex-col justify-between gap-9">
           <Text variant="uppercase">{name}</Text>
           <GarmentDescription product={product} />
-
           <div className="space-y-5">
             <Modal
               product={product}
