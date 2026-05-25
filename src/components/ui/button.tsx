28bdeea4c7501de8700b79531c181a984af110b3
0101oak <115372729+0101oak@users.noreply.github.com>
2026-05-25 16:53:50 +0300

    rm outline for btn component (#550)

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,113 +5,116 @@ import { sendButtonEvent } from "@/lib/analitycs/button";
 
 import { Loader } from "./loader";
 
-export const buttonVariants = cva("disabled:cursor-not-allowed block", {
-  variants: {
-    variant: {
-      default: ["text-textBaseSize", "disabled:opacity-50"],
-      simple: [
-        "text-bgColor",
-        "bg-textColor",
-        "hover:bg-bgColor",
-        "hover:text-textColor",
-      ],
-      simpleReverse: ["text-textColor", "bg-bgColor"],
-      simpleReverseWithBorder: [
-        "text-textColor",
-        "bg-bgColor",
-        "border",
-        "border-textColor",
-        "leading-4",
-      ],
-      main: [
-        "border",
-        "border-textColor",
-        "text-textBaseSize",
-        "text-bgColor",
-        "bg-textColor",
-        "hover:bg-bgColor",
-        "hover:text-textColor",
-        "disabled:bg-textInactiveColor",
-        "disabled:text-bgColor",
-        "disabled:border-textInactiveColor",
-        "leading-4",
-        "text-center",
-      ],
-      secondary: [
-        "border",
-        "border-textColor",
-        "text-textBaseSize",
-        "text-textColor",
-        "hover:bg-textColor",
-        "hover:text-bgColor",
-        "disabled:bg-textInactiveColor",
-        "disabled:bg-textInactiveColor",
-        "disabled:text-bgColor",
-        "disabled:border-textInactiveColor",
-        "leading-4",
-        "text-center",
-      ],
-      underline: ["underline", "disabled:text-textInactiveColor"],
-      underlineReverse: [
-        "text-bgColor",
-        "underline",
-        "disabled:text-textInactiveColor",
-      ],
-      underlineWithColors: [
-        "underline",
-        "text-highlightColor",
-        "disabled:text-textInactiveColor",
-        "visited:text-visitedLinkColor",
-      ],
-      strikeThrough: [
-        "relative",
-        "text-textBaseSize",
-        "text-textInactiveColor",
-        "before:absolute",
-        "before:top-1/2",
-        "before:left-0",
-        "before:w-full",
-        "before:h-[1px]",
-        "before:-translate-y-1/2",
-        "before:content-['']",
-        "before:bg-[var(--inactive)]",
-        "disabled:cursor-not-allowed",
-      ],
+export const buttonVariants = cva(
+  "block disabled:cursor-not-allowed focus:outline-none focus:ring-0",
+  {
+    variants: {
+      variant: {
+        default: ["text-textBaseSize", "disabled:opacity-50"],
+        simple: [
+          "text-bgColor",
+          "bg-textColor",
+          "hover:bg-bgColor",
+          "hover:text-textColor",
+        ],
+        simpleReverse: ["text-textColor", "bg-bgColor"],
+        simpleReverseWithBorder: [
+          "text-textColor",
+          "bg-bgColor",
+          "border",
+          "border-textColor",
+          "leading-4",
+        ],
+        main: [
+          "border",
+          "border-textColor",
+          "text-textBaseSize",
+          "text-bgColor",
+          "bg-textColor",
+          "hover:bg-bgColor",
+          "hover:text-textColor",
+          "disabled:bg-textInactiveColor",
+          "disabled:text-bgColor",
+          "disabled:border-textInactiveColor",
+          "leading-4",
+          "text-center",
+        ],
+        secondary: [
+          "border",
+          "border-textColor",
+          "text-textBaseSize",
+          "text-textColor",
+          "hover:bg-textColor",
+          "hover:text-bgColor",
+          "disabled:bg-textInactiveColor",
+          "disabled:bg-textInactiveColor",
+          "disabled:text-bgColor",
+          "disabled:border-textInactiveColor",
+          "leading-4",
+          "text-center",
+        ],
+        underline: ["underline", "disabled:text-textInactiveColor"],
+        underlineReverse: [
+          "text-bgColor",
+          "underline",
+          "disabled:text-textInactiveColor",
+        ],
+        underlineWithColors: [
+          "underline",
+          "text-highlightColor",
+          "disabled:text-textInactiveColor",
+          "visited:text-visitedLinkColor",
+        ],
+        strikeThrough: [
+          "relative",
+          "text-textBaseSize",
+          "text-textInactiveColor",
+          "before:absolute",
+          "before:top-1/2",
+          "before:left-0",
+          "before:w-full",
+          "before:h-[1px]",
+          "before:-translate-y-1/2",
+          "before:content-['']",
+          "before:bg-[var(--inactive)]",
+          "disabled:cursor-not-allowed",
+        ],
+      },
+      isLoading: {
+        true: ["relative overflow-hidden"],
+        false: [],
+      },
+      size: {
+        sm: ["text-textBaseSize"],
+        default: ["text-textBaseSize"],
+        lg: ["py-2.5", "px-4", "text-textBaseSize"],
+        giant: [
+          "py-10",
+          "px-16",
+          "text-textGiantSmallSize",
+          "lg:text-textGiantSize",
+        ],
+      },
     },
-    isLoading: {
-      true: ["relative overflow-hidden"],
-      false: [],
-    },
-    size: {
-      sm: ["text-textBaseSize"],
-      default: ["text-textBaseSize"],
-      lg: ["py-2.5", "px-4", "text-textBaseSize"],
-      giant: [
-        "py-10",
-        "px-16",
-        "text-textGiantSmallSize",
-        "lg:text-textGiantSize",
-      ],
+    compoundVariants: [
+      {
+        variant: "main",
+        isLoading: true,
+      },
+      {
+        variant: "secondary",
+        isLoading: true,
+        className:
+          "disabled:!bg-textColor disabled:!border-textColor disabled:!text-bgColor",
+      },
+    ],
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+      isLoading: false,
     },
   },
-  compoundVariants: [
-    {
-      variant: "main",
-      isLoading: true,
-    },
-    {
-      variant: "secondary",
-      isLoading: true,
-      className:
-        "disabled:!bg-textColor disabled:!border-textColor disabled:!text-bgColor",
-    },
-  ],
-  defaultVariants: {
-    variant: "default",
-    size: "default",
-    isLoading: false,
-  },
-});
+);
 
 interface Props extends VariantProps<typeof buttonVariants> {
   asChild?: boolean;
